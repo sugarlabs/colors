6898a2ce1533723a14ed09337ac86041492a04d2
olpc <olpc@xo-05-26-CC.localdomain>
2010-01-31 16:53:52 +0000

    Update setupwacom.sh to get wacom.ko from a location that works. Remove unused NEWS file.

--- setupwacom.sh
+++ setupwacom.sh
@@ -5,7 +5,7 @@ yum -y install linuxwacom
 
 echo Copying and installing Wacom kernel module...
 mkdir /lib/modules/`uname -r`/kernel/drivers/usb/input
-wget http://dev.laptop.org/~pgf/wacom.ko -O /lib/modules/`uname -r`/kernel/drivers/usb/input/wacom.ko
+wget http://dev.laptop.org/~wadeb/wacom.ko -O /lib/modules/`uname -r`/kernel/drivers/usb/input/wacom.ko
 
 echo Copying Xorg configuration file...
 wget http://dev.laptop.org/~wadeb/xorg-dcon.conf -O /etc/X11/xorg-dcon.conf
